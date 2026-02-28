aecc0727220e9d534405342fd06d6f834e7342b5
maxengna <phanupong.w2019@gmail.com>
2026-02-28 15:26:05 +0700

    fix host

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    host: true,
-    port: 4173
+  server:{
+    host: true
+  },
+  preview:{
+    allowHosts: true,
+    host: ['todolist.app.com']
   }
+  // plugins: [react()],
+  // preview: {
+  //   host: true,
+  //   port: 4173
+  // }
 })
