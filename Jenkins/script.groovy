984ad6fb97cd6dcd87f1dc24f031d4ff7649a779
maxengna <phanupong.w2019@gmail.com>
2026-03-01 14:54:43 +0700

    test cicd time2

--- Jenkins/script.groovy
+++ Jenkins/script.groovy
@@ -65,7 +65,7 @@ def updateK8s() {
 }
 
 def githubPush() {
-    echo 'Pushing K8s manifests to github1'
+    echo 'Pushing K8s manifests to github2'
 
     withCredentials([usernamePassword(
     credentialsId: 'github-token',
