04454508c3eebcab8b48664be7ecdb9e5715434f
maxengna <phanupong.w2019@gmail.com>
2026-03-02 10:59:31 +0700

    change vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  server:{
-    host: true
-  },
-  preview:{
-    allowHosts: true,
-    host: ['todolist.app.com']
+  preview: {
+    host: true,
+    allowedHosts: ['todolist.app.com']
   }
-  // plugins: [react()],
-  // preview: {
-  //   host: true,
-  //   port: 4173
-  // }
 })
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   server:{
+//     host: true
+//   },
+//   preview:{
+//     allowHosts: true,
+//     host: ['todolist.app.com']
+//   }
+//   // plugins: [react()],
+//   // preview: {
+//   //   host: true,
+//   //   port: 4173
+//   // }
+// })
