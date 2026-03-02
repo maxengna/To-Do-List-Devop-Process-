4de5a816604af97cf98fde97e8a57f4485df717f
maxengna <phanupong.w2019@gmail.com>
2026-03-02 15:36:30 +0700

    inject plugin

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,26 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
 export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true
+  },
   preview: {
     host: true,
     allowedHosts: ['todolist.app.com']
   }
 })
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   server:{
-//     host: true
-//   },
-//   preview:{
-//     allowHosts: true,
-//     host: ['todolist.app.com']
-//   }
-//   // plugins: [react()],
-//   // preview: {
-//   //   host: true,
-//   //   port: 4173
-//   // }
-// })
