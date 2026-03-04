ec68397d8816ea3e140acee405f61eceadee0aff
maxengna <phanupong.w2019@gmail.com>
2026-03-04 16:04:07 +0700

    change pass

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST || "mysql",   // ใช้ service name ใน k8s
   port: 3306,
   user: process.env.DB_USER || "demo",
-  password: process.env.DB_PASSWORD || "dGVzdHVzZXJkZW1v",
+  password: process.env.DB_PASSWORD || "maxdev789",
   database: process.env.DB_NAME || "todolist",
   waitForConnections: true,
   connectionLimit: 10,
