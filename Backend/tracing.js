334f980c20f525578552d57a82d766a80ff9260b
maxengna <phanupong.w2019@gmail.com>
2026-03-13 18:28:28 +0700

    test cicd

--- Backend/tracing.js
+++ Backend/tracing.js
@@ -6,7 +6,7 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
-    url: "http://otel-collector:4318/v1/traces"
+    url: "http://otel-collector:4318/v1/traces"  // otel-collector is the service name of the opentelemetry-collector in the k8s cluster
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 })
